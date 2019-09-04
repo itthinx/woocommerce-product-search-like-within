c81f68688b6e8a7b27125e332f13602e14fffdf9
itthinx <itthinx@gmail.com>
2019-09-04 18:42:07 +0200

    release

---
+++ woocommerce-product-search-like-within.php
@@ -0,0 +1,42 @@
+<?php
+/**
+* Plugin Name: WooCommerce Product Search Like Within
+* Plugin URI:
+* Description: Enables search within terms for <a href=" https://www.itthinx.com/shop/woocommerce-product-search/">WooCommerce Product Search</a>.
+* Version: 1.0.0
+* Author: itthinx
+* Author URI: http://www.itthinx.com
+* Donate-Link: http://www.itthinx.com
+* License: GPLv3
+*/
+
+if ( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Implements the woocommerce_product_search_like_within filter to enable search within.
+ */
+class WooCommerce_Product_Search_Like_Within {
+
+	/**
+	 * Adds the woocommerce_product_search_like_within filter.
+	 */
+	public static function boot() {
+		add_filter( 'woocommerce_product_search_like_within', 'enable_wps_search_like_within', 10, 3 );
+	}
+
+	/**
+	 * Enable search within.
+	 *
+	 * @param boolean $enable
+	 * @param array $object_type_ids
+	 * @param array $search_terms
+	 *
+	 * @return boolean
+	 */
+	public static function enable_wps_search_like_within( $enable, $object_type_ids, $search_terms ) {
+		return true;
+	}
+}
+WooCommerce_Product_Search_Like_Within::boot();
